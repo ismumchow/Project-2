269e6b386ed44b086b00f85935cff9d058729c4f
ischdry <ichowdhury4@fordham.edu>
2019-02-14 18:46:30 -0500

    updated

---
+++ models/reply.js
@@ -0,0 +1,28 @@
+
+module.exports = function(sequelize, DataTypes) {
+  var Reply = sequelize.define("Reply", {
+
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+      //text allows an almost unlimited size
+    }
+  });
+
+  Reply.associate = function(models) {
+  
+    Reply.belongsTo(models.Post, {
+      foreignKey: {
+        allowNull: false
+      }
+    }),
+    Reply.belongsTo(models.Author, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Reply;
+};
